cd225d940cbcae9b0c427cc6d9cf21727141e5bc
Pavel Merekin <merekin33@gmail.com>
2025-02-11 10:36:50 +0700

    binary search throw recursion added

--- algorithms-realization/02-binary.js
+++ algorithms-realization/02-binary.js
@@ -2,6 +2,7 @@
 
 const sortedArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
+// через цикл
 function binarySearch(arr, item) {
   let start = 0;
   let end = arr.length;
@@ -28,4 +29,21 @@ function binarySearch(arr, item) {
   return `Position "${-1}", element "${item}" not found`;
 }
 
-console.log(binarySearch(sortedArray, 6));
+// console.log(binarySearch(sortedArray, 6));
+
+// рекурсивный способ
+function recursiveBinarySearch(arr, item, start, end) {
+  let middle = Math.floor((start + end) / 2);
+
+  if (item === arr[middle]) {
+    return middle;
+  }
+
+  if (item < arr[middle]) {
+    return recursiveBinarySearch(arr, item, 0, middle - 1);
+  } else {
+    return recursiveBinarySearch(arr, item, middle + 1, end);
+  }
+}
+
+// console.log(recursiveBinarySearch(sortedArray, 6, 0, sortedArray.length));
